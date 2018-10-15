214ccf0c2923b6b5719a02c34b9f3ccd50829a9f
James Hunt <jhunt@socnet.soc.plymouth.ac.uk>
2018-10-15 16:47:27 +0100

    all model classes created - moving onto controller classes

--- ProductOrderingSystem/ProductOrderingSystem/Models/Order.cs
+++ ProductOrderingSystem/ProductOrderingSystem/Models/Order.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProductOrderingSystem.Models
 {
     public class Order
     {
-        public int                      Order_Number        { get; set; }
-        public DateTime                 Order_Date          { get; set; }
-        public ICollection<Order_Line>  Order_Lines         { get; set; }
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int                                  Order_Number        { get; set; }
+        public  DateTime                            Order_Date          { get; set; }
+        public virtual ICollection<Order_Line>      Order_Lines         { get; set; }
     }
 }
---
+++ ProductOrderingSystem/ProductOrderingSystem/ProductDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProductOrderingSystem
+{
+    public class ProductDB : DbContext
+    {
+        public DbSet<Models.Product>        Products        { get; set; }
+        public DbSet<Models.Order>          Orders          { get; set; }
+        public DbSet<Models.Order_Line>     Order_Lines     { get; set; }
+    }
+}
